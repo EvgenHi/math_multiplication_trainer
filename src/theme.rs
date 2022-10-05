d38769d4faaccd133dfc5316337b6e6f5210a88a
TerraBoii <kostyaborisenko05@gmail.com>
2022-10-05 13:35:09 +0300

    Some theming done

--- src/theme.rs
+++ src/theme.rs
@@ -20,25 +20,26 @@ impl CustomTheme {
             CustomTheme::Red => Color32::from_rgba_premultiplied(189, 46, 54, 235),
             CustomTheme::Green => Color32::from_rgba_premultiplied(106, 232, 110, 235),
             CustomTheme::Transparent => Color32::from_rgba_premultiplied(0, 0, 0, 0),
-            CustomTheme::WindowsXP => Color32::from_rgba_premultiplied(59, 119, 188, 235),
+            CustomTheme::WindowsXP => Color32::from_rgba_premultiplied(212, 208, 200, 255),
         }
     }
 
     pub fn text_color(&self) -> Color32 {
         match self {
             CustomTheme::Dark => Color32::from_rgba_premultiplied(150, 150, 150, 255),
-            CustomTheme::Light => Color32::from_rgba_premultiplied(105, 105, 105, 255),
-            CustomTheme::Blue => Color32::from_rgba_premultiplied(100, 100, 100, 255),
-            CustomTheme::Red => Color32::from_rgba_premultiplied(150, 150, 150, 255),
-            CustomTheme::Green => Color32::from_rgba_premultiplied(100, 100, 100, 255),
-            CustomTheme::Transparent => Color32::from_rgba_premultiplied(180, 180, 180, 255),
-            CustomTheme::WindowsXP => Color32::from_rgba_premultiplied(129, 192, 70, 235),
+            CustomTheme::Light => Color32::from_rgba_premultiplied(0, 0, 0, 255),
+            CustomTheme::Blue => Color32::from_rgba_premultiplied(0, 0, 0, 255),
+            CustomTheme::Red => Color32::from_rgba_premultiplied(0, 0, 0, 255),
+            CustomTheme::Green => Color32::from_rgba_premultiplied(0, 0, 0, 255),
+            CustomTheme::Transparent => Color32::from_rgba_premultiplied(215, 215, 215, 255),
+            CustomTheme::WindowsXP => Color32::from_rgba_premultiplied(0, 0, 0, 255),
         }
     }
 
     // "stroke" is these line around user_input text box
     pub fn stroke_standart(&self) -> Color32 {
         match self {
+            CustomTheme::WindowsXP => Color32::from_rgba_premultiplied(61, 149, 255, 255),
             _ => Color32::from_rgba_premultiplied(193, 223, 255, 255),
         }
     }
